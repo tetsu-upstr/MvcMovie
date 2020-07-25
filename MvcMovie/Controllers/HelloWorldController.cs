755c45121002389866711364b8e2df96b410282d
tetsu-upstr <bodomu@gmail.com>
2020-07-25 14:05:31 +0900

    create HelloWorldController

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // HTTP GETメソッド ベースURLに/HelloWorld/を追加することで呼び出される
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+    }
+}
